42e5942836c3897f78fd6aff78497a28fd759221
ST10303285 <ST10303285@vcconnect.edu.za>
2024-06-27 21:48:57 +0200

    Added comments to the InputDialogScaling.xaml.cs

--- InputDialogScalingRecipe.xaml.cs
+++ InputDialogScalingRecipe.xaml.cs
@@ -15,9 +15,9 @@ using System.Windows.Shapes;
 namespace RecipeApplicationWPF
 {
   
-    public partial class InputDialogScalingRecipe : Window
+    public partial class InputDialogScalingRecipe : Window //This class is used to create a dialog box for the user to input the scaling factor for the recipe
     {
-        public InputDialogScalingRecipe()
+        public InputDialogScalingRecipe() //Constructor for the class
         {
             InitializeComponent();
             
@@ -25,28 +25,28 @@ namespace RecipeApplicationWPF
         
           public string Message
          {
-                 get { return MessageLabel.Content.ToString(); }
-                 set { MessageLabel.Content = value; }
+                 get { return MessageLabel.Content.ToString(); }//This property is used to get the message that is displayed in the dialog box
+                 set { MessageLabel.Content = value; }//This property is used to set the message that is displayed in the dialog box
          }  
 
-         public string Input { get; private set; }  
+         public string Input { get; private set; }  //This property is used to get the input from the user
          private void OKButton_Click(object sender, RoutedEventArgs e)
         {
             Input = InputTextBox.Text;
-            DialogResult = true;
+            DialogResult = true;//This property is used to set the dialog result to true
         }
 
-        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        private void CancelButton_Click(object sender, RoutedEventArgs e) //This method is used to close the dialog box
         {
             DialogResult = false;
         }
 
-        public string RecipeTitleInput
+        public string RecipeTitleInput //This property is used to get the input from the user
         {
             get { return InputTextBox.Text; }
         }
 
-        public string ScalingFactorInput
+        public string ScalingFactorInput //This property is used to get the input from the user
         {
             get { return InputTextBox.Text; }
         }
